bd7b7482e90b480012f6c4c133372df132ebd9c2
ydzly <ydglitch@gmail.com>
2025-03-18 16:11:46 +0000

    added user context

---
+++ app/util/providers.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { auth } from "@/lib/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+
+const UserContext = React.createContext();
+export const useUser = () => React.useContext(UserContext);
+
+export const userProvider = ({ children }) => {
+  const [user, setUser] = React.useState({});
+  React.useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      }
+    });
+  }, user);
+
+  return <UserContext.Provider value={user}>{children}</UserContext.Provider>;
+};
