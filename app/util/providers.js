fe6b5fa564ac2be7a5f40e3052ec29e578ae4d30
Ydeezly <74550749+dglitxh@users.noreply.github.com>
2025-03-18 16:18:39 +0000

    Update providers.js

--- app/util/providers.js
+++ app/util/providers.js
@@ -10,7 +10,11 @@ export const userProvider = ({ children }) => {
   React.useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
-        setUser(user);
+        setUser({
+          email: user.email,
+          uid: user.uid,
+          displayName: user.displayName,
+        });
       }
     });
   }, user);
